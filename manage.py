e3b92a4a0e023d24fa7d9af90448b338155c2caf
Paedar Rader <113387714+paedarr@users.noreply.github.com>
2025-04-22 15:00:32 -0400

    Adjustment changes
    adding manage.py and SQL database

---
+++ manage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import subprocess
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'staging.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+        
+    if 'runserver' in sys.argv:
+        subprocess.Popen(["python3", "manage.py", "run_roomgpt"])
+        
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
